73c9b4394eb9805ccca263c3111b9359c36637c0
augmity <augmity@augmity.com>
2019-12-08 00:10:53 -0800

    feat: added search functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import 'bulma/css/bulma.css';
 import './App.css';
 
+import { NasaImagesApiHook } from "./hooks/nasaImagesApi.hook";
+
+
 function App() {
+
+  let searchInput;
+  const [{ data, isLoading, isError }, search] = NasaImagesApiHook();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">NASA Images Search</h1>
+          <h2 className="subtitle">
+            Type something (try "mars" or "earth") in the input below and click "Search" 🙂
+          </h2>
+
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                ref={input => searchInput = input}
+                disabled={isLoading ? 'disabled' : ''}
+                onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    search(searchInput.value);
+                  }
+                }}
+              />
+            </div>
+            <div className="control">
+              <button className={`button is-success ${isLoading ? 'is-loading' : ''}`} onClick={() => search(searchInput.value)}>Search</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container">
+
+          <div className="image-grid">
+            {data && data.images && data.images.map(image => (
+              <img src={image.uri} key={image.id} />        
+            ))}            
+          </div>
+
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ src/hooks/nasaImagesApi.hook.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+export const NasaImagesApiHook = () => {
+
+  const nasaImageDataToImageData = (data) => {
+    // We have to make sure that the dat object returned by the server has all the properties we need to access
+    if (data && data.collection && data.collection.items) {
+      // Iterating over the images array
+      return data.collection.items
+        .map((item, index) => {
+          if (item.links) {
+            // We need to find an object in the 'link' array of type 'preview
+            const link = item.links.find(link => link.rel === 'preview');
+            return (link) ? { id: index.toString(), uri: link.href } : null;
+          } else {
+            return null;
+          }
+        })
+        .filter(item => item !== null) // Because (even that it shouldn't) some items after mapping may be null
+    } else {
+      return null;
+    }
+  }
+
+
+  const [data, setData] = useState({});
+  const [query, setQuery] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      setData({});
+      try {
+        const result = await axios(`https://images-api.nasa.gov/search?q=${query}`);
+        setData({ images: nasaImageDataToImageData(result.data) });
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+
+    if (query) {
+      fetchData();
+    }
+  }, [query]);
+
+  return [{ data, isLoading, isError }, setQuery];
+}
